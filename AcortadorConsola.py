674e46acf755e6f9868ceda8eab50031b983a3b4
yilberg <61481753+YilberG@users.noreply.github.com>
2022-03-15 15:34:08 -0500

    cambiando acortador de forma loca

--- AcortadorConsola.py
+++
@@ -1,9 +0,0 @@
-import pyshorteners
-
-url = input("ingresar el link >>>")
-
-shrt = pyshorteners.Shortener()
-
-nurl = shrt.tinyurl.short(url)
-
-print(nurl)
--- index.py
+++ index.py
@@ -1,7 +1,14 @@
 from flask import Flask,render_template, request, redirect, url_for
-import mysql.connector
-import pyshorteners
+from random import choice
+import mysql.connector,string
 
+# db = mysql.connector.connect(
+#     host='127.0.0.1',
+#     user='root',
+#     password='',
+#     database='url',
+#     port=3306
+# )
 db = mysql.connector.connect(
     host='academia.c1mebdhdxytu.us-east-1.rds.amazonaws.com',
     user='p3',
@@ -17,15 +24,11 @@ app = Flask(__name__)
 def index():
     cursor = db.cursor(dictionary=True)
 
+    cursor.execute('select nueva from urls order by id desc limit 1')
+    ultima = cursor.fetchone()
 
-    cursor.execute('select * from urls')
-
-    urls = cursor.fetchall()
-
-    cursor.execute('SELECT MAX(nueva) AS nueva FROM urls')
-    ultima = cursor.fetchall()
     cursor.close()
-    return render_template("index.html", urls=urls, ultima = ultima)
+    return render_template("index.html", ultima = ultima)
 
 @app.get("/shorturl")
 def shortUrl():
@@ -34,35 +37,35 @@ def shortUrl():
 @app.post("/shorturl")
 def shortUrlPost():
     nombreurl = request.form.get('url')
-
-    shrt = pyshorteners.Shortener()
-
-    nurl = shrt.tinyurl.short(nombreurl)
+    codigo = ''.join(choice(string.ascii_letters+string.digits) for _ in range(4))
+    nueva = request.host_url+codigo # http://127.0.0.1:5000/dsWN
 
     cursor = db.cursor()
-
-    cursor.execute("insert into urls(url, nueva) values(%s, %s)",(
+    cursor.execute("insert into urls(url,nueva) values(%s,%s)",(
         nombreurl,
-        nurl
+        nueva,
     ))
+
     cursor.close()
     return redirect(url_for('index'))
 
-@app.get("/tablaurl")
-def tablaurl():
-
+@app.get("/tabla")
+def tablaUrl():
     cursor = db.cursor(dictionary=True)
-
-
     cursor.execute('select * from urls')
-
     urls = cursor.fetchall()
 
-    cursor.execute('SELECT MAX(nueva) AS nueva FROM urls')
-    ultima = cursor.fetchall()
     cursor.close()
-    return render_template("tablaurl.html", urls=urls, ultima = ultima)
+    return render_template("tablaurl.html", urls=urls)
 
+@app.get("/<codigo>")
+def redirectnueva(codigo):
+    shorturl = request.host_url+codigo # http://127.0.0.1:5000/dsWN
+    cursor = db.cursor()
+    cursor.execute("select url from urls where nueva = %s", (shorturl,))
+    url = cursor.fetchone()
+    if(url != None): return redirect(url[0])
+    return redirect(url_for('index'))
 
-#app.run(debug=True)
+app.run(debug=True)
 
